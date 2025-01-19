238e2829224cb433be41f92e69bac3647b3393a6
Sarah Schneider <mureb_rosa@hotmail.com>
2025-01-18 22:01:51 -0300

    add styles

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,12 @@ Rails.application.configure do
   # Enable server timing.
   config.server_timing = true
 
+  # Config assets
+  config.assets.debug = true
+  config.assets.check_precompiled_asset = false
+  config.assets.compile = true
+
+
   # Enable/disable Action Controller caching. By default Action Controller caching is disabled.
   # Run rails dev:cache to toggle Action Controller caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
